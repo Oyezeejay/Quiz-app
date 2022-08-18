e99bb6506f8b094a29661848fb3f23ca36454b98
Zarak Jahan <108816986+Oyezeejay@users.noreply.github.com>
2022-08-18 10:46:59 +0530

    first commit

---
+++ js/questions.js
@@ -0,0 +1,68 @@
+let questions = [
+    {
+    numb: 1,
+    question: "The term ‘Computer’ is derived from……….?",
+    answer: "Latin",
+    options: [
+      "English",
+      "Latin",
+      "Spanish",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The malware that fetches the personal data such as bank details, credit/card details, password, etc. from remotely located computers is known as …",
+    answer: "Trojans",
+    options: [
+      "Trojans",
+      "Virus",
+      "Spyware",
+      "Worms"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+  numb: 6,
+    question: "The Third Generation Computer used …",
+    answer: "Integrated circuit",
+    options: [
+      "Transistors",
+      "Vacuum tube",
+      "Chip",
+      "Integrated circuit"
+    ]
+  }
+];
